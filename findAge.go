75f4867f2a4b0cd1700817c2c29500dc23ee856c
Ali <hubealii1570@gmail.com>
2022-09-04 16:33:34 +0500

    first commit

---
+++ findAge.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	var currentyear (int) = 2022
+	var birthyear (int) = 0
+
+	fmt.Println("Enter the year of birth:")
+	fmt.Scan(&birthyear)
+
+	age := currentyear - birthyear
+
+	fmt.Println("Your age is:", age)
+
+}
